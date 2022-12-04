c777d7900840c59f6b4eefdcd6ad3310b42710d9
StahlbrinkM <stahlbrink.marcus@gmail.com>
2022-12-04 19:47:04 +0100

    Added total cost and refresh features

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import PizzaIcon from "./images/pizza.png"
 import HamburgerIcon from "./images/hamburger.png"
 import BeerIcon from "./images/beer.png"
+import checkMark from "./images/checkMark.png"
 import { useState } from "react";
 
 function App() {
 
   const [listItems, setListItems] = useState([])
   const [isVisable, setIsVisable] = useState(false)
+  const [totalPrice, setTotalPrice] = useState([])
+  const [finishedOrder, setFinishedOrder] = useState(false)
 
   const removeItems = (index) => {
     const newListItems = listItems.filter((_, i) => i !== index);
     setListItems(newListItems)
   }
 
+  const addFoodData = (itemFood, itemPrice) => {
+    setListItems((prevItems) => [...prevItems, itemFood]);
+    setTotalPrice((prevTotal) => [...prevTotal, itemPrice]);
+  };
+
+  const changeModal = () => {
+    setIsVisable(prevVisable => !prevVisable);
+    setFinishedOrder(prevOrder => !prevOrder);
+
+    setTimeout(() => {setFinishedOrder((prevOrder => !prevOrder))}, 3000)
+    setTimeout(() => { window.location.reload(false)}, 3000)
+
+  }
 
 
   return (
-    <div className="bg-gray-200 h-full">
+    <div className="bg-gray-200 h-auto">
       {/* ----- Container ----- */}
       <div className='w-4/12 italic flex-col m-auto bg-white'>
         {/* ----- Header -----*/}
@@ -34,7 +50,7 @@ function App() {
               <p className='text-xs'>pepperoni ,mushroom, mozzarella</p>
               <p className='text-lg'>$14</p>
             </div>
-            <button onClick={() => setListItems((prevItems) => [...prevItems, "Pizza"])} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
+            <button onClick={() => addFoodData("Pizza", 14)} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
           </div>
           <div className='my-7 pb-5 m-auto flex justify-center px-2 w-11/12 border-b border-black'>
             <img src={HamburgerIcon} alt="hamburger icon" className="w-12 object-contain"/>
@@ -43,7 +59,7 @@ function App() {
               <p className='text-xs'>beef, cheese, lettuce</p>
               <p className='text-lg'>$12</p>
             </div>
-            <button onClick={() => setListItems((prevItems) => [...prevItems, "Hamburger"])} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
+            <button onClick={() => addFoodData("Hamburger", 12)} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
           </div>
           <div className='my-5 pb-5 m-auto flex justify-center px-2 w-11/12 border-b border-black'>
             <img src={BeerIcon} alt='beer icon' className="w-12 object-contain"/>
@@ -52,7 +68,7 @@ function App() {
               <p className='text-xs'>grain, hops, yeast, water</p>
               <p className='text-lg'>$12</p>
             </div>
-            <button onClick={() => setListItems((prevItems) => [...prevItems, "Beer"])} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
+            <button onClick={() => addFoodData("Beer", 12)} className="ml-auto mr-6 border-2 border-black rounded-full w-16 h-16">+</button>
           </div>
         </div>
         {/* ----- Main Area ----- */}
@@ -62,9 +78,28 @@ function App() {
             {listItems.length >= 1 ?
           <div>
             <p className="text-center">Your order</p>
+
             {listItems.map((item, index) => {
-              return <div key={index} className="flex px-8"><p className="text-xl">{item}</p><div><button onClick={() => removeItems(index)} className="text-xs font-thin ml-4 text-gray-500">remove</button></div><p className="ml-auto mr-6">${item === "Pizza" ? 14 : 12}</p></div>
+              return <div key={index} className="flex px-8">
+                <p className="text-xl">
+                  {item}
+                </p>
+                <div>
+                  <button onClick={() => removeItems(index)} className="text-xs font-thin ml-4 text-gray-500">
+                    remove
+                  </button>
+                </div>
+                  <p className="ml-auto mr-6">
+                    ${item === "Pizza" ? 14 : 12}
+                  </p>
+                </div>
             })}
+
+
+            <div className="px-8 border-t border-black mt-4 pt-4 flex">
+              <p className="text-xl">Total price:</p>
+              <p className="ml-auto mr-6">${totalPrice.reduce((a,b) => a + b, 0)}</p>
+            </div>
             
             <button onClick={() => setIsVisable(prevVisable => !prevVisable)} className="bg-green-400 h-12 w-4/5 ml-10 mt-4 rounded text-center">Complete order</button>
           </div>
@@ -82,11 +117,21 @@ function App() {
             <input className="block ml-10 pl-4 mt-2 border border-black rounded h-12 w-4/5" type="text" placeholder="Enter CVV"></input>
           </div>
 
-          <button onClick={() => setIsVisable(prevVisable => !prevVisable)} className="bg-green-400 h-12 w-4/5 ml-10 mt-6 rounded text-center">Pay</button>
+          <button onClick={() => changeModal()} className="bg-green-400 h-12 w-4/5 ml-10 mt-6 rounded text-center">Pay</button>
 
 
         </div> 
         : ""}
+
+        {finishedOrder && !isVisable ? 
+          <div className="w-[25rem] h-96 fixed bg-white inset-x-6 inset-y-10 flex-col border border-black m-auto">
+            <div className="justify-center align-center items-center pt-10">
+              <p className="text-3xl text-center">Your order is on it's way!</p>
+              <img className="w-2/4 mx-auto mt-10" src={checkMark} alt="finished order"/>
+            </div>
+          </div>
+          : ""
+        }
         {/* ----- Modal ----- */}
 
       </div>
